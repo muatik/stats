a7fb0d56dbe2441f3abf652ce462aa7cf6164e6a
muatik <muatik@gmail.com>
2017-09-12 20:34:13 +0300

    adds summary implementation and its unittests

--- src/main/java/com/muatik/entity/Summary.java
+++ src/main/java/com/muatik/entity/Summary.java
@@ -4,4 +4,34 @@ package com.muatik.entity;
  * Created by muatik on 9/12/17.
  */
 public class Summary {
+    private long count = 0;
+    private double sum = 0;
+    private double avg = 0;
+
+    public Summary(long count, double sum, double avg) {
+        this.count = count;
+        this.sum = sum;
+        this.avg = avg;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public double getAvg() {
+        return avg;
+    }
+
+    @Override
+    public String toString() {
+        return "Summary{" +
+                "count=" + count +
+                ", sum=" + sum +
+                ", avg=" + avg +
+                '}';
+    }
 }
---
+++ src/test/java/com/muatik/SummaryTest.java
@@ -0,0 +1,40 @@
+package com.muatik;
+
+import com.muatik.entity.Summary;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by muatik on 9/12/17.
+ */
+@RunWith(SpringRunner.class)
+public class SummaryTest {
+
+    @Test
+    public void testSummaryConstructor() {
+        long count = 20;
+        double sum = 1002;
+        double avg = 50.1;
+
+        Summary stat = new Summary(count, sum, avg);
+        Assert.assertEquals(avg, stat.getAvg(), 0);
+        Assert.assertEquals(count, stat.getCount(), 0);
+        Assert.assertEquals(sum, stat.getSum(), 0);
+    }
+
+    @Test
+    public void testSummaryToString() {
+        long count = 20;
+        double sum = 1002;
+        double avg = 50.1;
+
+        Summary stat = new Summary(count, sum, avg);
+        Assert.assertEquals(
+                "Summary{count=20, sum=1002.0, avg=50.1}",
+                stat.toString());
+    }
+
+
+}
